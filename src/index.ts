4dd0fc1afe04024498f33ce24ae529219171d869
aszabacsi <akos.szabacsi@gmail.com>
2018-05-03 23:57:32 +0200

    cluster added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import * as cluster from 'cluster';
 import * as http from 'http';
+import * as os from 'os';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.write('Success');
-  res.end();
-});
+if(cluster.isMaster) {
 
-server.listen(process.env.PORT || 8000);
+  const n = os.cpus().length;
+
+  for(let i = 0; i < n; ++i) {
+    cluster.fork();
+  }
+
+  cluster.on('error', (worker, code, signal) => {
+    console.log(
+      `Worker died PID=${worker.process.pid} CODE=${code} SIGNAL=${signal}`
+    );
+  });
+
+} else {
+
+  const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.write('Success');
+    res.end();
+  });
+  
+  server.listen(process.env.PORT || 8000);
+
+}
+
