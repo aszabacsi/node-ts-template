b36c6f545e106adcfc0b72154478ba2dedb16315
aszabacsi <akos.szabacsi@gmail.com>
2018-05-07 08:19:33 +0200

    added graphql

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,45 @@
 import * as cluster from 'cluster';
 import * as http from 'http';
 import * as os from 'os';
+import * as w from 'winston';
+import * as express from 'express';
+import { buildSchema } from 'graphql';
+import * as gql from 'express-graphql';
 
-if(cluster.isMaster) {
+if (cluster.isMaster) {
 
   const n = os.cpus().length;
 
-  for(let i = 0; i < n; ++i) {
+  for (let i = 0; i < n; i = i + 1) {
     cluster.fork();
   }
 
   cluster.on('error', (worker, code, signal) => {
-    console.log(
-      `Worker died PID=${worker.process.pid} CODE=${code} SIGNAL=${signal}`
-    );
+    w.error(`Worker died PID=${worker.process.pid} CODE=${code} SIGNAL=${signal}`);
   });
 
 } else {
 
-  const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.write('Success');
-    res.end();
+  const schema = buildSchema(`
+    type Query {
+      test: String
+    }
+  `);
+
+  const root = {
+    test: () => 'test',
+  };
+
+  const app = express();
+
+  app.use('/api', gql({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }));
+
+  app.listen(process.env.PORT || 8000, () => {
+    w.info('Server is running...');
   });
-  
-  server.listen(process.env.PORT || 8000);
 
 }
-
